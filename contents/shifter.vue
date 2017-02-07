374d481e101ec7a9c2f69f3df27478ada345630c
justin lee <yuminjustin@163.com>
2017-02-07 11:31:28 +0800

    shifter调用实例

---
+++ contents/shifter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+     <p>开关</p>
+	 <Shifter :value="value" :clickFun="click"></Shifter>
+	 <p>您选择了{{text}}</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Shifter from '../components/shifter'
+
+export default {
+  data(){
+    return {
+	  value:!0,
+	  text:"开启"
+	}
+  },
+  components: {
+     Shifter
+  },
+  methods:{
+     click(v){
+	    this.value = v;
+	 }
+  },
+  watch:{
+     value(v){
+	    this.text = v?"开启":"关闭"
+	 }
+  }
+}
+</script>
