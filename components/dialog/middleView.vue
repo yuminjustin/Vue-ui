f84941fd68c4d9ef4a0462be2a2123dadd7a0df8
justin lee <yuminjustin@163.com>
2017-02-10 15:54:37 +0800

    视图转换组件

---
+++ components/dialog/middleView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="_dialog_content">
+    <div class="_dialog_title">
+      <p>{{title}}</p>
+      <span class="glyphicon glyphicon-remove" @click="close" :title="closeText"></span>
+    </div>
+    <div class="_dialog_box" v-bind:is="currentView">
+      I'm a Dialog
+    </div>
+    <div class="_dialog_foot">
+      <button v-show="hasSureBtn" type="button" class="btn btn-warning" @click="submit">
+		{{sureText}}
+	  </button>
+      <button v-show="hasCancelBtn" type="button" class="btn btn-default" @click="cancel">
+		{{cancelText}}
+	  </button>
+    </div>
+  </div>
+</template>
