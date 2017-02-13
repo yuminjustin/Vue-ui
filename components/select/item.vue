3365d2241ae3bc99d12e5c35a8ee828f87bb770b
justin lee <yuminjustin@163.com>
2017-02-13 10:58:32 +0800

    单个选项

---
+++ components/select/item.vue
@@ -0,0 +1,13 @@
+<template>
+  <li :value="item.id" @click="$emit('select',item)" :title="item.name">
+    <a :href="item.url">{{item.name}}</a>
+    <span v-show="muti" class="glyphicon glyphicon-ok"></span>
+  </li>
+</template>
+<script>
+  export default {
+    name: 'Item',
+    props: ['item', 'muti']
+  }
+
+</script>
