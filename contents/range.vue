75ae6b094b0e7e6374c77127155a15680c41f355
justin lee <yuminjustin@163.com>
2017-02-14 15:46:44 +0800

    range调用实例

---
+++ contents/range.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Range :start="0" :stop="100" :step="1"/>
+  </div>
+</template>
+<script>
+  import Range from "../components/range"
+  export default {
+    components:{
+      Range
+    }
+  }
+
+</script>
